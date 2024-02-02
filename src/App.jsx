9d8da558480d5785398624c7d0185702659ec2eb
Kevin Phon <kevinphon@Kevins-MacBook-Air.local>
2024-02-01 16:55:27 -0800

    add index.css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import Header from "./compontnes/header/Header";
+import Nav from "./compontnes/nav/Nav";
+import About from "./compontnes/about/About";
+import Experience from "./compontnes/experience/Experience";
+import Services from "./compontnes/services/Services";
+import Portfolio from "./compontnes/portfolio/Portfolio";
+import Testimonials from "./compontnes/testimonials/Testimonials";
+import Contact from "./compontnes/contact/Contact";
+import Footer from "./compontnes/footer/Footer";
 
 function App() {
   return (
-    <div>Hello world</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/compontnes/services/Services.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import './servies.css'
+
+export default function Services() {
+  return <div>Services</div>;
+}
